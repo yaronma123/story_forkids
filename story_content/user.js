38569f0b596ae095f851203f08abca519e43cd6b
yaronma123 <mairzik@gmail.com>
2022-05-27 11:28:06 +0300

    story_forkids
    storylineexe

---
+++ story_content/user.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6K2C8d4XJBo":
+        Script1();
+        break;
+      case "68G60BhDd0a":
+        Script2();
+        break;
+      case "6dPgdMtfnA1":
+        Script3();
+        break;
+      case "6GFPc6uBrMA":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script3()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script4()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
